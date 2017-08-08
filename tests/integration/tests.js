7a5147ec4e5e4ced041d4a3d74af65b53908ace7
Chris Woodall <christopherhwoodall@gmail.com>
2017-08-08 16:57:00 +0100

    all tests complete

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -52,7 +52,101 @@ it('can I add with it', function() {
   expect(running_total.getAttribute('value')).to.eventually.equal('9')
 })
 
+it('can I subtract with it', function() {
+  running_total = element(by.css('#running_total'))
+  element(by.css('#number5')).click();
+  element(by.css('#operator_subtract')).click();
+  element(by.css('#number4')).click();
+  element(by.css('#operator_equals')).click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('1')
+})
+
+it('can I multiply with it', function() {
+  running_total = element(by.css('#running_total'))
+  element(by.css('#number5')).click();
+  element(by.css('#operator_multiply')).click();
+  element(by.css('#number4')).click();
+  element(by.css('#operator_equals')).click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('20')
+})
+
+it('can I divide with it', function() {
+  running_total = element(by.css('#running_total'))
+  element(by.css('#number8')).click();
+  element(by.css('#operator_divide')).click();
+  element(by.css('#number2')).click();
+  element(by.css('#operator_equals')).click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('4')
+})
+
+//Can we chain multiple operations together?
+
+it('can I do everything at once with it', function() {
+  running_total = element(by.css('#running_total'))
+  element(by.css('#number1')).click();
+  element(by.css('#operator_add')).click();
+  element(by.css('#number5')).click();
+  element(by.css('#operator_subtract')).click();
+  element(by.css('#number1')).click();
+  element(by.css('#operator_multiply')).click();
+  element(by.css('#number2')).click();
+  element(by.css('#operator_divide')).click();
+  element(by.css('#number2')).click();
+  element(by.css('#operator_equals')).click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('5')
+})
+
+// Does it work as expected for a range of numbers?negative, decimals, very large numbers)
+it('can I do negative numbers', function() {
+  running_total = element(by.css('#running_total'))
+  element(by.css('#number1')).click();
+  element(by.css('#operator_subtract')).click();
+  element(by.css('#number9')).click();
+  element(by.css('#operator_equals')).click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('-8')
+})
+
+it('can I do decimal numbers', function() {
+  running_total = element(by.css('#running_total'))
+  element(by.css('#number1')).click();
+  element(by.css('#operator_divide')).click();
+  element(by.css('#number4')).click();
+  element(by.css('#operator_equals')).click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('0.25')
+})
+
+it('can I do very large numbers', function() {
+  running_total = element(by.css('#running_total'))
+  element(by.css('#number9')).click();
+  element(by.css('#operator_multiply')).click();
+  element(by.css('#number9')).click();
+  element(by.css('#operator_multiply')).click();
+  element(by.css('#number9')).click();
+  element(by.css('#operator_multiply')).click();
+  element(by.css('#number9')).click();
+  element(by.css('#operator_multiply')).click();
+  element(by.css('#number9')).click();
+  element(by.css('#operator_multiply')).click();
+  element(by.css('#number9')).click();
+  element(by.css('#operator_multiply')).click();
+  element(by.css('#number9')).click();
+  element(by.css('#operator_multiply')).click();
+  element(by.css('#number9')).click();
+  element(by.css('#operator_multiply')).click();
+  element(by.css('#number9')).click();
+  element(by.css('#operator_equals')).click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('387420489')
+})
+
+// If you divide by zero, what is the effect?
+it('can I divide by 0', function() {
+  running_total = element(by.css('#running_total'))
+  element(by.css('#number1')).click();
+  element(by.css('#operator_divide')).click();
+  element(by.css('#number0')).click();
+  element(by.css('#operator_equals')).click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('undefined')
+})
 
 
-
-});
+})
